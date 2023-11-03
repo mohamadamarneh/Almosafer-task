563a43e9934a9b47027c599dff3b117ae8607954
mohamad amarneh <mohamadamarneh7@gmail.com>
2023-11-03 19:38:30 +0300

    make report

--- cypress.config.js
+++ cypress.config.js
@@ -2,22 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "g9az8i",
-  viewportWidth: 1080,
-  viewportHeight: 720,
-  
- 
-
-
-
-
+  reporter: 'cypress-mochawesome-reporter',
+  //video: true,
   e2e: {
     baseUrl: 'https://www.almosafer.com/',
-
-
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      screenshotOnRunFailure: true;
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+    //screenshotOnRunFailure: true,
+
+    viewportWidth: 1080,
+    viewportHeight: 720,
+
     testIsolation: false,
   },
 });
